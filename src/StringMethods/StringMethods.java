eff600ec7223fc44752ce3e3704b4f5df4ef81e3
Derek <Derek@windows10.microdone.cn>
2021-10-04 18:19:45 -0700

    String Methods

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -32,13 +32,19 @@ public class StringMethods {
 
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		return null;
+		if(s1.length()>s2.length()) {
+			return s1;
+		}
+		return s2;
 	}
 
 	
 	// if String s contains the word "underscores", change all of the spaces to underscores
 	public static String formatSpaces(String s) {
-		return null;
+		if(s.contains("underscores")) {
+			return s.replaceAll("\\s", "_");
+		}
+		return s;
 	}
 
 	
@@ -46,36 +52,82 @@ public class StringMethods {
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
-		return null;
+		String s1a = s1.trim();
+		String s2a = s2.trim();
+		String s3a = s3.trim();
+		String s1LastName = s1a.substring(s1a.indexOf(" ") + 1, s1a.length());
+		String s2LastName = s2a.substring(s2a.indexOf(" ") + 1, s2a.length());
+		String s3LastName = s3a.substring(s3a.indexOf(" ") + 1, s3a.length());
+		
+		if(s1LastName.compareTo(s2LastName)<0 && s1LastName.compareTo(s3LastName)<0) {
+			return s1a;
+		}
+		else if(s1LastName.compareTo(s2LastName)>0 && s2LastName.compareTo(s3LastName)<0) {
+			return s2a;
+		}
+	
+		return s3a;
 	}
 	
 	
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+		String number = "";
+		int add = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if(Character.isDigit(s.charAt(i))) {
+				number = s.charAt(i) + "";
+				add += Integer.parseInt(number);
+			}
+		}
+		return add;
 	}
 	
 	
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
-		return 0;
+		int counter = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if(s.charAt(i)==' ') {
+				s.replaceAll(" ", "");
+			}
+		}
+		
+		for (int i = 0; i < s.length()-substring.length()+1; i++) {
+		String sub = s.substring(i, i+substring.length());	
+		if(sub.equals(substring)) {
+			counter++;
+		}
+		}
+		return counter;
 	}
 
 	// Call Utitilities.encrypt to encrypt String s
 	public static String encrypt(String s, char key) {
-		return null;
+		return Utilities.encrypt(s.getBytes(), (byte)key);
+		
 	}
 
 	// Call Utilities.decrypt to decrypt the cyphertext
 	public static String decrypt(String s, char key) {
-		return null;
+		return Utilities.decrypt(s, (byte)key);
 	}
 
 
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
-		return 0;
+			int counter = 0;
+			String[] words = s.split(" ");
+
+			
+			for (String i : words) {
+				if(i.endsWith(substring)) {
+					counter++;
+				}
+			}
+			return counter;
+		
 	}
 	
 
@@ -83,7 +135,8 @@ public class StringMethods {
 	// of String substring and the final occurrence
 	// You can assume that substring will appear at least twice
 	public static int distance(String s, String substring) {
-		return 0;
+		return s.lastIndexOf(substring) - (s.indexOf(substring) + substring.length());
+
 	}
 
 
@@ -91,7 +144,35 @@ public class StringMethods {
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
-		return true;
+		s.trim();
+		s = s.replaceAll("[\\W_]", "");
+		s = s.toLowerCase();
+		String sub1 = s.substring(0, s.length()/2+1);
+		String sub2 = s.substring(s.length()/2,s.length());
+		String half1 = s.substring(0, s.length()/2-1);
+		String half2 = s.substring(s.length()/2+1,s.length());
+		String reverse = "";
+		for (int i = sub2.length()-1; i >=0; i--) {
+			reverse = reverse + sub2.charAt(i);
+		}
+		//System.out.println(sub1);
+		//System.out.println(reverse);
+		//System.out.println(s);
+		//System.out.println(sub1 + " " + reverse);
+		//System.out.println(half1 + " " + half2);
+		if((s.length())%2==1) {
+			if(sub1.equals(reverse)) {
+				return true;
+			}
+			
+		}
+		else {
+			if(half1.equals(half2)) {
+				return true;
+			}
+		}
+		
+		return false;	
 	}
 	
 }
